0a8c011a0079a0f36d212309c0f9fe3ab7673167
nohier <noiherassan2@gmail.com>
2016-08-06 12:45:51 +0200

    lesson 3 not refreshing kan sh3'al oxem blah

--- app/src/main/java/com/example/mohamedk/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mohamedk/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mohamedk.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,8 +44,13 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
+
+
+
         }
 
         return super.onOptionsItemSelected(item);
--- app/src/main/java/com/example/mohamedk/myapplication/MainActivityFragment.java
+++ app/src/main/java/com/example/mohamedk/myapplication/MainActivityFragment.java
@@ -1,8 +1,11 @@
 package com.example.mohamedk.myapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -12,6 +15,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -44,11 +48,29 @@ public class MainActivityFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Add this line in order for this fragment to handle menu events.
+        FetchWeatherTask f = new FetchWeatherTask();
+        f.execute(getString(R.string.locatoin_pref_default_value));
         setHasOptionsMenu(true);
     }
     @Override
+    public void onStart(){
+        super.onStart();
+        updateWeather();
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.forecastfragment, menu);
+    }
+    private void updateWeather(){
+
+        FetchWeatherTask f = new FetchWeatherTask();
+        // now we want to get the value saved in the preferences
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String location = prefs.getString(getString(R.string.location_pref_key),
+                getString(R.string.locatoin_pref_default_value));
+        f.execute(location);
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
@@ -56,9 +78,10 @@ public class MainActivityFragment extends Fragment {
 
         int id = item.getItemId();
         if(id == R.id.action_refresh) {
-            FetchWeatherTask f = new FetchWeatherTask();
-            f.execute("94043");
+            updateWeather();
+
         }
+
 
 
             return super.onOptionsItemSelected(item);
@@ -95,6 +118,15 @@ public class MainActivityFragment extends Fragment {
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
         return rootView;
     }
 
@@ -124,10 +156,24 @@ public class MainActivityFragment extends Fragment {
          * Prepare the weather high/lows for presentation.
          */
         private String formatHighLows(double high, double low) {
-            // For presentation, assume the user doesn't care about tenths of a degree.
+
+            // here we want to check for the check for the unites selected by the user
+            SharedPreferences sharedprefs =
+                    PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = sharedprefs.getString(
+                    getString(R.string.temp_pref_key),getString(R.string.temp_lable_metric)
+            );
+            if(unitType.equals(getString(R.string.temp_value_imperial))){
+                high = (high*1.8)+32;
+                low  =(low*1.8)+32;
+            }
+            else if(!unitType.equals(getString(R.string.temp_value_metric))) // if the value nither nor
+               Log.d(LOG_TAG,"Unit type not found"+unitType);
+
+
+// For presentation, assume the user doesn't care about tenths of a degree.
             long roundedHigh = Math.round(high);
             long roundedLow = Math.round(low);
-
             String highLowStr = roundedHigh + "/" + roundedLow;
             return highLowStr;
         }
